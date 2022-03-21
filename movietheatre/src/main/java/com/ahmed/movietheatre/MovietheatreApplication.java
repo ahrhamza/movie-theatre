54021e27c34ab6caf217fa67387efce0668e61e1
ahrhamza <ahmedhamza.riaz@gmail.com>
2022-03-20 18:33:35 -0700

    Initial commit with empty project

---
+++ movietheatre/src/main/java/com/ahmed/movietheatre/MovietheatreApplication.java
@@ -0,0 +1,13 @@
+package com.ahmed.movietheatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovietheatreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovietheatreApplication.class, args);
+	}
+
+}
